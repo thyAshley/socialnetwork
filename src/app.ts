79b3b87f79c39b0e91821d26d3198d9418a481bc
Ashley <thy.ashley@gmail.com>
2020-07-26 22:06:43 +0800

    build for heroku

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -16,15 +17,20 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", (req, res, next) => {
-  res.send("API Running");
-});
 
 // Routes
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/profile", profileRoute);
+
+if (process.env.NODE_ENV === "production") {
+  // Set Statoc Fp;der
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Connect Node Appplication
 app.listen(PORT, () => {
