e1a34e960028b6c5575a2d4b72432e1a32b45868
Ashley <thy.ashley@gmail.com>
2020-07-26 22:00:30 +0800

    ready for deployment on heroku

---
+++ client/build/precache-manifest.f43914966468ea2714140409f5302dfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82861263da4182b4b043fc799d1dc1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf52d4b236dd0631522a",
+    "url": "/static/css/main.208d6d07.chunk.css"
+  },
+  {
+    "revision": "801a42e34d0abf9c4abf",
+    "url": "/static/js/2.808ae594.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.808ae594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf52d4b236dd0631522a",
+    "url": "/static/js/main.6e5335ee.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
